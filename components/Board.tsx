534d87a7f5bcd5fe358a0944574cb0995d8d35b9
DonaldIvan <developer.donaldivan@gmail.com>
2023-06-04 20:20:11 +0800

    Fetch board

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
+import { useBoardStore } from '@/store/BoardStore';
+
 const Board = () => {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  console.log('board', board);
   return (
     // <DragDropContext>
     //   <Droppable droppableId="board" direction="horizontal" type="column">
